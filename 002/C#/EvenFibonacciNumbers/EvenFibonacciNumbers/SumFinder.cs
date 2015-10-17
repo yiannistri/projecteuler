1eb0fc03dee9287cbb38bfeeaa37e0c4fee05176
yiannis <yiannis.triantafyllopoulos@gmail.com>
2015-10-17 22:07:42 +0100

    C# solution for problem 002

---
+++ 002/C#/EvenFibonacciNumbers/EvenFibonacciNumbers/SumFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacciNumbers
+{
+    public class SumFinder
+    {
+        public int Calculate(int max, Func<int, bool> filter)
+        {
+            return Fibonacci(max).Where(filter).Sum();
+        }
+
+        private IEnumerable<int> Fibonacci(int max)
+        {
+            int previous = 1;
+            int current = 2;
+            var list = new List<int> { previous, current };
+            int sum = previous + current;
+            while (sum <= max)
+            {
+                list.Add(sum);
+                previous = current;
+                current = sum;
+                sum = previous + current;
+            }
+            return list.AsEnumerable();
+        }
+    }
+}
---
+++ 002/C#/EvenFibonacciNumbers/EvenFibonacciNumbers/SumFinderTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace EvenFibonacciNumbers
+{
+    public class SumFinderTests
+    {
+        [Fact]
+        public void When_values_less_than_100_result_is_44()
+        {
+            Assert.Equal(44, new SumFinder().Calculate(100, x => x % 2 == 0));
+        }
+
+        [Fact]
+        public void When_values_less_than_4000000_result_is_4613732()
+        {
+            Assert.Equal(4613732, new SumFinder().Calculate(4000000, x => x % 2 == 0));
+        }
+    }
+}
