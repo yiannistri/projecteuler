3a74e43585cd0af712f3c66c4ffd181a8810a052
yiannis <yiannis.triantafyllopoulos@gmail.com>
2015-10-17 21:23:39 +0100

    C# solution for problem 001

---
+++ 001/C#/MultiplesOf3And5/MultiplesOf3And5/SumFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplesOf3And5
+{
+    public class SumFinder
+    {
+        public int Calculate(int max, Func<int, bool> filter)
+        {
+            return Enumerable.Range(0, max).Where(filter).Sum();
+        }
+    }
+}
---
+++ 001/C#/MultiplesOf3And5/MultiplesOf3And5/SumFinderTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace MultiplesOf3And5
+{
+    public class SumFinderTests
+    {
+        [Fact]
+        public void When_max_is_10_result_is_23()
+        {
+            Assert.Equal(23, new SumFinder().Calculate(10, x => x % 3 == 0 || x % 5 == 0));
+        }
+
+        [Fact]
+        public void When_max_is_1000_result_is_233168()
+        {
+            Assert.Equal(233168, new SumFinder().Calculate(1000, x => x % 3 == 0 || x % 5 == 0));
+        }
+    }
+}
